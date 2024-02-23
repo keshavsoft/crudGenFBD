094f2de9ae42b3b96d661bcfed6338de0e210d11
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-02-23 19:30:33 +0530

    SREENIVA

--- public/LaundryPos/Dashboard/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
+++ public/LaundryPos/Dashboard/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncToFactory } from "./ToFactory/Entry.js";
+import { StartFunc as StartFuncOrdersId } from "./OrdersId/Entry.js";
 
 const StartFunc = () => {
     StartFuncToFactory();
+    StartFuncOrdersId();
 };
 
 export { StartFunc };
