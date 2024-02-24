5d652a54dae449ee5fba8e686f27dde0d954b1c7
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-02-24 12:26:25 +0530

    SREENIVAS

--- public/LaundryPos/ToFactory/QrCodes/BSTableFromScreen/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
+++ public/LaundryPos/ToFactory/QrCodes/BSTableFromScreen/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
@@ -1,16 +1,15 @@
 import { StartFunc as StartFuncBSTableURL } from "./ParentId/Entry.js";
 import { StartFunc as StartFuncDashboardId } from "./DashboardId/Entry.js";
 import { StartFunc as StartFuncBSTableFromScreen } from "./BSTableFromScreen/EntryFile.js";
-import { StartFunc as StartFuncTree } from "./Tree/Entry.js";
-import { StartFunc as StartFuncKSTree } from "./KSTree/Entry.js";
+// import { StartFunc as StartFuncTree } from "./Tree/Entry.js";
+import { StartFunc as StartFuncTotalQrs } from "./KSTree/Entry.js";
 import { StartFunc as StartFuncCreate } from "./Create/Entry.js";
 
 const StartFunc = () => {
     StartFuncBSTableURL();
     StartFuncDashboardId();
     StartFuncBSTableFromScreen();
-    StartFuncTree();
-    StartFuncKSTree();
+    StartFuncTotalQrs();
     StartFuncCreate();
 };
 
---
+++ public/LaundryPos/ToFactory/QrCodes/TotalQrs/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -0,0 +1,29 @@
+
+let StartFunc = ({ inDataToShow }) => {
+    let LocalDataToShow = inDataToShow;
+    console.log("LocalDataToShow:",LocalDataToShow);
+    let jVarLocalBranchName = getUrlQueryParams({ inGetKey: "BranchName" });
+    let LocalFilterScanedData = LocalDataToShow.filter(e => e.BookingData.OrderData.BranchName == jVarLocalBranchName);
+    if ((LocalFilterScanedData.length > 0) === false) swal.fire({ title: "No data !", icon: "error" });
+
+    jFLocalHideSpinner();
+    var $table = $('#table');
+
+    $table.bootstrapTable("destroy").bootstrapTable({
+        data: LocalFilterScanedData,
+    });
+};
+
+let jFLocalHideSpinner = () => {
+    let jVarLocalSpinnerId = document.getElementById("SpinnerId");
+    jVarLocalSpinnerId.style.display = "none";
+};
+
+let getUrlQueryParams = ({ inGetKey }) => {
+    const queryString = window.location.search;
+    const parameters = new URLSearchParams(queryString);
+    const value = parameters.get(inGetKey);
+    return value;
+};
+
+export { StartFunc }
