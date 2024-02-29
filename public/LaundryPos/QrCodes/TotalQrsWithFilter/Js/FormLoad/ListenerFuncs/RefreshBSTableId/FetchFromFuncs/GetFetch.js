d13b1944324aeb1bf5423df8a491e971c26d090c
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-02-29 15:44:47 +0530

    SREENIVAS

--- public/LaundryPos/QrCodes/TotalQrsWithFilter/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/GetFetch.js
+++ public/LaundryPos/QrCodes/TotalQrsWithFilter/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/GetFetch.js
@@ -1,10 +1,9 @@
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = `/bin/QrCodes/Generate`;
+    let jVarLocalFetchUrl = `/bin/Factory/QrCodeDetails`;
     let response = await fetch(jVarLocalFetchUrl);
     let data = await response.json();
 
     return await data;
 };
-
 
 export { StartFunc };
