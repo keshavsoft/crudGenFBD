d71243f53ffe9afb1b81ae5d35da0bcade148f61
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-03-05 16:29:05 +0530

    SREENIVAS

--- public/LaundryPos/Orders/QrCodesTable/Js/Navigation/OrdersShow/EntryFile.js
+++
@@ -1,8 +0,0 @@
-import { StartFunc as StartFuncURLNavigation } from "./URLNavigation.js";
-
-let StartFunc = () => {
-    let jVarLocalNavId = document.getElementById("OrderShowId");
-    jVarLocalNavId.addEventListener("click", StartFuncURLNavigation)
-};
-
-export { StartFunc };
