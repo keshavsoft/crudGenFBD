ef689e90b0020337e87ff05aeff6d800949d1bd2
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-03-05 21:00:45 +0530

    SREENIVAS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { router as routerFromSrc } from "./src/routes.js";
 import { router as routerFromBin } from "./bin/routes.js";
 import { router as routerFromCustom } from "./Custom/routes.js";
+import { router as routerFromCommon } from "./Common/routes.js";
 
 import { StartFunc as StartFuncKWSServer } from "./Projects/KWSServer/EntryFile.js";
 import { StartFunc as StartFuncPortListen } from "./PortListen.js";
@@ -46,6 +47,7 @@ app.get('/AboutUs', (req, res) => {
 app.use('/src', routerFromSrc);
 app.use('/bin', routerFromBin);
 app.use('/Custom', routerFromCustom);
+app.use('/Common', routerFromCommon);
 
 StartFuncKWSServer(server);
 
