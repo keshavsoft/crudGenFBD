f48bdd9bf4f2b38f739688cc7302035a248d2363
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-03-05 10:56:29 +0530

    SREENIVAS

--- public/LaundryPos/Masters/Customers/Create/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ public/LaundryPos/Masters/Customers/Create/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,24 +1,21 @@
 let StartFunc = ({ inFetchData }) => {
     let LocalinFetchData = inFetchData;
-    
-    if (LocalinFetchData.KTF === false) {
-        Swal.fire({
-            title: `${LocalinFetchData.KReason}`,
-            confirmButtonText: "ok",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = ""
-                return;
-            };
-        });
-        return;
-    };
 
-    const url = new URL(window.location.href);
-    const params1 = new URLSearchParams(url.search);
-    let NewURl = new URL("../Show/Show.html", url);
-    const new_url = new URL(`${NewURl.href}?${params1}`);
-    window.location.href = new_url.href;
+    if (LocalinFetchData.KTF === false) {
+
+        let jVarLocalBranchName = document.getElementById('Mobile');
+        jVarLocalBranchName.classList.add("is-invalid");
+        document.getElementById("MobileClass").innerHTML = "Duplicate"
+        jVarLocalBranchName.focus();
+        return;
+    } else {
+
+        const url = new URL(window.location.href);
+        const params1 = new URLSearchParams(url.search);
+        let NewURl = new URL("../Show/Show.html", url);
+        const new_url = new URL(`${NewURl.href}?${params1}`);
+        window.location.href = new_url.href;
+    };
 };
 
 export { StartFunc }
