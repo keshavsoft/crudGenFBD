02f6c7d984af8f3a8933e73d4cc8c91a62cd5bfa
Srivasu215 <sri.vasu215@gmail.com>
2024-03-09 19:18:02 +0530

    SREENIVAS

--- public/LaundryFactory/Dashboard/Js/AddListeners/NavMenu/Entry.js
+++ public/LaundryFactory/Dashboard/Js/AddListeners/NavMenu/Entry.js
@@ -12,8 +12,9 @@ let jFLocalToURL = ({ inRowPk }) => {
     const url = new URL(window.location.href);
     const params1 = new URLSearchParams(url.search);
     let NewURl = new URL("../FromBranch/Scan/BSTableFilterFromVoucher/BSTableFilterFromVoucher.html", url);
-    NewURl.searchParams.set('VoucherRef', LocalinRowPk);
     const new_url = new URL(`${NewURl.href}?${params1}`);
+    new_url.searchParams.append('VoucherRef', LocalinRowPk);
+
     window.location.href = new_url.href;
 
 };
