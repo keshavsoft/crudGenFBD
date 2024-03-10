e9e2661fde0ae43cf746257da8bdc664020e8978
keshavsoft <nknnkumar@yahoo.com>
2024-03-10 21:46:48 +0530

    keshav

--- public/LaundryPos/Masters/Customers/ShowWithInfo/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
+++ public/LaundryPos/Masters/Customers/ShowWithInfo/Js/FormLoadBeforeAdmin/NavMenu/Entry.js
@@ -2,7 +2,7 @@ import { StartFunc as StartFuncBSTableURL } from "./ParentId/Entry.js";
 import { StartFunc as StartFuncDashboardId } from "./DashboardId/Entry.js";
 import { StartFunc as StartFuncShow } from "./BSTableFromScreen/EntryFile.js";
 import { StartFunc as StartFuncCreate } from "./Tree/Entry.js";
-import { StartFunc as StartFuncShowWithInfoId } from "./ShowWithInfoId/EntryFile.js";
+import { StartFunc as StartFuncKSTree } from "./KSTree/Entry.js";
 // import { StartFunc as StartFuncCreate } from "./Create/Entry.js";
 
 const StartFunc = () => {
@@ -10,8 +10,6 @@ const StartFunc = () => {
     StartFuncDashboardId();
     StartFuncShow();
     StartFuncCreate();
-
-    StartFuncShowWithInfoId();
     // StartFuncKSTree();
     // StartFuncCreate();
 };
