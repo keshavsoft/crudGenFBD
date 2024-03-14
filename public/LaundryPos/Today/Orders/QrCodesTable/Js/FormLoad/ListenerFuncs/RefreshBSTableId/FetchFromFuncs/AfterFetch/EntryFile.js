0358d46d2b95a92ff00c210daa8c67f2941f2309
Srivasu215 <sri.vasu215@gmail.com>
2024-03-14 12:43:48 +0530

    SREENIVAS

--- public/LaundryPos/Today/Orders/QrCodesTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ public/LaundryPos/Today/Orders/QrCodesTable/Js/FormLoad/ListenerFuncs/RefreshBSTableId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -50,6 +50,7 @@ let jFLocalInsertQrCodeData = ({ inData, inQrCodeData }) => {
 
     let jVarLocalReturnArray = [];
     inData.forEach(element => {
+        element.TimeSpan = jFLocalKInterval({ inCurrentdateandtime: element.OrderData.Currentdateandtime })
         element.IsQrCodesRaised = false;
         element.TotalItems = 0;
         let FilterCheck = inQrCodeData.filter(ele => ele.OrderNumber == element.pk && ele.BookingData.OrderData.BranchName == jVarLocalBranchName);
@@ -68,6 +69,20 @@ let getUrlQueryParams = ({ inGetKey }) => {
     const parameters = new URLSearchParams(queryString);
     const value = parameters.get(inGetKey);
     return value;
+};
+
+let jFLocalKInterval = ({ inCurrentdateandtime }) => {
+    if (inCurrentdateandtime === undefined) {
+        return "";
+    }
+    var diffMs = (new Date() - new Date(inCurrentdateandtime)); // milliseconds between now & Christmas
+    var diffDays = Math.floor(diffMs / 86400000); // days
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+
+    return diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...";
+
+    // console.log(diffDays + " days, " + diffHrs + " hours, " + diffMins + " min...");
 };
 
 
