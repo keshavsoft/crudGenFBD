a99a518dc42692f925bbdde721957b0980302676
Keshav <nknnkumar@yahoo.com>
2024-03-16 01:04:23 +0530

    keshav

--- public/LaundryPos/Masters/Items/Charts/RateBar/js/formLoad/Addlisteners/RefreshButtonClick/CommonCode/ShowTable.js
+++ public/LaundryPos/Masters/Items/Charts/RateBar/js/formLoad/Addlisteners/RefreshButtonClick/CommonCode/ShowTable.js
@@ -1,23 +1,3 @@
-const StartFunc1 = () => {
-    let jVarLocalDataFromLocalStorage = localStorage.getItem("tableData");
-
-    let LocalColumnsKeysArray = JSON.parse(jVarLocalDataFromLocalStorage);
-
-    let LocalDataToShow = LocalColumnsKeysArray;
-    
-    if ((LocalDataToShow.length > 0) === false) swal.fire({ title: "No data !", icon: "error" });
-
-    // jFLocalHideSpinner();
-    var $table = $('#table');
-
-    $table.bootstrapTable("destroy").bootstrapTable({
-        data: jFSortData({ inDataToSort: LocalDataToShow }),
-        onPostBody: function () {
-            $(".fixed-table-toolbar .search .search-input").focus()
-        }
-    });
-};
-
 const StartFunc = () => {
     let jVarLocalDataFromLocalStorage = localStorage.getItem("tableData");
 
