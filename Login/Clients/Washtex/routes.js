e9acc26a35df37027fef9f661998024fa406b2a8
Keshav <nknnkumar@yahoo.com>
2024-03-17 22:52:30 +0530

    keshav

--- Login/Clients/Washtex/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-var router = express.Router();
-
-import { router as routerFromQrcodes } from './Qrcodes/routes.js';
-
-router.use('/Qrcodes', routerFromQrcodes);
-
-export { router };
