a51e55620024d63842afcfb742c5bf6ef5e6ebac
Srivasu215 <sri.vasu215@gmail.com>
2024-04-02 20:19:36 +0530

    SREENIVAS

--- public/Factory/Washing/Voucher/Js/FormLoad/FromUrlParams/EntryFile.js
+++ public/Factory/Washing/Voucher/Js/FormLoad/FromUrlParams/EntryFile.js
@@ -10,7 +10,30 @@ let StartFunc = () => {
         });
     };
 
-    document.getElementById("FactoryNameId").innerHTML = jVarLocalFactoryName;
+    jFShowFactoryNameId({ inValue: jVarLocalFactoryName })
+    jFShowDeliveryDateTime();
+    jFShowFactory({ inValue: jVarLocalFactoryName })
+};
+
+const jFShowFactoryNameId = ({ inValue }) => {
+    let jVarLocalHtmlId = "FactoryNameId";
+    let jVarLocalMenItemsTabId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalMenItemsTabId.innerHTML = inValue;
+};
+
+const jFShowDeliveryDateTime = () => {
+    let jVarLocalHtmlId = "Date";
+    let currentDate = new Date();
+    let formattedDate = currentDate.toISOString().slice(0, 10);
+
+    let jVarLocalMenItemsTabId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalMenItemsTabId.value = formattedDate;
+};
+
+const jFShowFactory = ({ inValue }) => {
+    let jVarLocalHtmlId = "Factory";
+    let jVarLocalMenItemsTabId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalMenItemsTabId.value = inValue;
 };
 
 export { StartFunc }
