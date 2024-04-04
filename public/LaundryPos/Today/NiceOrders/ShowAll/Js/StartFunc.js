f8da8301e061cf1ee045110a748a659c5ecfc1a8
Srivasu215 <sri.vasu215@gmail.com>
2024-04-04 16:32:47 +0530

    SREENIVAS

--- public/LaundryPos/Today/NiceOrders/ShowAll/Js/StartFunc.js
+++ public/LaundryPos/Today/NiceOrders/ShowAll/Js/StartFunc.js
@@ -1,7 +1,5 @@
-import { StartFunc as StartFuncNavigation } from "./Navigation/EntryFile.js";
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
-import { StartFunc as StartFuncAfterDomLoad } from "./AfterDomLoad/StartFunc.js";
 
 const StartFunc = () => {
     // StartFuncNavigation();
@@ -11,9 +9,7 @@ const StartFunc = () => {
     if (jVarLocalFromAdmin) {
         StartFuncFormLoad();
 
-        StartFuncShowOnDom({}).then(() => {
-            StartFuncAfterDomLoad();
-        });
+        StartFuncShowOnDom({}).then();
     };
 };
 
