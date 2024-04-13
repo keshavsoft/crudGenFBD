bbe0493b280c2c118fdf085a50d097e06d2dc828
Srivasu215 <sri.vasu215@gmail.com>
2024-04-13 18:10:01 +0530

    SREENIVAS

--- public/Laundry/Branch/Orders/NewOrder/AddItems/Js/ShowToDom/EntryFile.js
+++ public/Laundry/Branch/Orders/NewOrder/AddItems/Js/ShowToDom/EntryFile.js
@@ -2,6 +2,18 @@ import { StartFunc as StartFuncToTable } from "./ToTable/EntryFile.js";
 
 const StartFunc = () => {
     StartFuncToTable();
+    jFdate();
 };
+
+const jFdate = () => {
+
+    var sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    var formattedSevenDaysAgo = sevenDaysAgo.getFullYear() + '-' + ((sevenDaysAgo.getMonth() + 1) < 10 ? '0' : '') + (sevenDaysAgo.getMonth() + 1) + '-' + (sevenDaysAgo.getDate() < 10 ? '0' : '') + sevenDaysAgo.getDate();
+
+    document.getElementById('TableFooterdateInputId').value = formattedSevenDaysAgo;
+
+}
 
 export { StartFunc }
