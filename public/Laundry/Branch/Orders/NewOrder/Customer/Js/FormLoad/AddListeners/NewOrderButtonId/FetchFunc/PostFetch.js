397b26d2f8210f2e12c86a838f4ce57fc89c09d4
keshavsoft <nknnkumar@yahoo.com>
2024-04-17 02:00:59 +0530

    keshav

--- public/Laundry/Branch/Orders/NewOrder/Customer/Js/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/PostFetch.js
+++ public/Laundry/Branch/Orders/NewOrder/Customer/Js/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/PostFetch.js
@@ -2,8 +2,11 @@ import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js"
 
 let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    console.log("jVarLocalFetchHeaders: ", jVarLocalFetchHeaders);
-    let jVarLocalFetchUrl = `/bin/Transactions/Kakinada`;
+    // console.log("jVarLocalFetchHeaders: ", jVarLocalFetchHeaders);
+
+    // let jVarLocalFetchUrl = `/bin/Transactions/Kakinada`;
+    let jVarLocalFetchUrl = "/Custom/Clients/Washtex/Orders/NewOrder/InsertNewOrder/Kakinada";
+
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
 
     return await response;
