b9d1fb099c377f65bd35d7fc775318fd7c897ba3
Keshav <nknnkumar@yahoo.com>
2024-04-21 16:09:27 +0530

    keshav

--- public/LaundryAdmin/Mazer/Masters/Customers/Delete/Js/FormLoad/BuildBSTables/onExpandRow/EntryFile.js
+++ public/LaundryAdmin/Mazer/Masters/Customers/Delete/Js/FormLoad/BuildBSTables/onExpandRow/EntryFile.js
@@ -1,23 +1,13 @@
-import { StartFunc as StartFuncOnExpandRow } from "./onExpandRow/EntryFile.js";
-
 let StartFunc = (index, row, $detail) => {
-   
-    // let jVarLocalDataArray = row.AggValues.ItemsArray;
     let jVarLocalTemplateForSubTable = document.getElementById("TemplateForRow");
     let clone = jVarLocalTemplateForSubTable.content.cloneNode("true");
 
-    clone.querySelector("#CustomerNameId").textContent  = "aaaaaaaaaaa";
-    console.log("clone : ",  clone.querySelector("#CustomerNameId"));
     const s = new XMLSerializer();
     const str = s.serializeToString(clone);
 
-    let jVarLocalInsideTable = $detail.html(str).find('table');
+    let jVarLocalCustomerNameId = $detail.html(str).find('#CustomerNameId');
 
-    // jVarLocalInsideTable.bootstrapTable({
-    //     data: jVarLocalDataArray,
-    //     detailView: true,
-    //     onExpandRow: StartFuncOnExpandRow
-    // });
+    jVarLocalCustomerNameId[0].value = row.CustomerName;
 };
 
 export { StartFunc };
