85c7942732b19800059ed35512b52bbf716cdc13
Srivasu215 <sri.vasu215@gmail.com>
2024-04-25 18:11:18 +0530

    SREENIVAS

--- public/Laundry/Branch/Orders/NewOrder/Customer/Js/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/FetchHeaders/EntryFile.js
+++ public/Laundry/Branch/Orders/NewOrder/Customer/Js/FormLoad/AddListeners/NewOrderButtonId/FetchFunc/FetchHeaders/EntryFile.js
@@ -5,9 +5,12 @@ let StartFunc = () => {
     let jVarLocalFromDom = jFLocalCustomerNameInputId();
     let jVarLocalMobileNumber = jVarLocalFromDom.split(":")[1];
     let jVarLocalCustomerName = jVarLocalFromDom.split(":")[0];
+    let jVarLocalBranchName = localStorage.getItem("BranchName");
+
 
     KeysJson.body.CustomerData.CustomerMobile = jVarLocalMobileNumber;
     KeysJson.body.CustomerData.CustomerName = jVarLocalCustomerName;
+    KeysJson.body.OrderData.BranchName = jVarLocalBranchName;
 
     KeysJson.body = JSON.stringify(KeysJson.body);
 
