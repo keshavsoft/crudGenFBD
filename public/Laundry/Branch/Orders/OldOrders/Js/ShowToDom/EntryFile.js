cbcb696eebc169f8ea2228180643b44fe3e6c960
bhaskar-459 <m03bhaskar@gmail.com>
2024-05-06 17:55:03 +0530

    added master

--- public/Laundry/Branch/Orders/OldOrders/Js/ShowToDom/EntryFile.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncToTable } from "./ToTable/EntryFile.js";
-import { StartFunc as StartFuncToHeading } from "./ToHeading/EntryFile.js";
-
-const StartFunc = () => {
-    StartFuncToTable();
-    StartFuncToHeading();
-};
-
-export { StartFunc };
