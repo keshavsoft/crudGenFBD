5167a3f4f9d0a9e7b7c78a5c747d6e1f2eb1c9ca
Srivasu215 <sri.vasu215@gmail.com>
2024-05-07 20:35:24 +0530

    SREENIVAS

--- public/Laundry/Branch/Orders/NewOrder/QrCodePrint/MultiPrintQr/Js/FormLoad/BuildBSTables/QrCodesTable/onClickRow/EntryFile.js
+++ public/Laundry/Branch/Orders/NewOrder/QrCodePrint/MultiPrintQr/Js/FormLoad/BuildBSTables/QrCodesTable/onClickRow/EntryFile.js
@@ -1,4 +1,5 @@
-import { StartFunc as StartFuncButtonClickFunc } from "./QRGenerate/2-ButtonClickFunc.js";
+// import { StartFunc as StartFuncButtonClickFunc } from "./QRGenerate/2-ButtonClickFunc.js";
+import { StartFunc as StartFuncButtonClickFunc } from "./MultipleQrCodeButtonId/2-ButtonClickFunc.js";
 
 const StartFunc = (row, $element, field) => {
     if (field === 8) {
